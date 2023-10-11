fa5de8659ac8eaa6cd20713cf9638f754d41387d
cheblimarc4 <cheblimarc4@gmail.com>
2023-10-11 15:12:18 +0200

    fix init with uat

--- iebank_api/__init__.py
+++ iebank_api/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
+from applicationinsights.flask.ext import AppInsights
 
 app = Flask(__name__)
 
@@ -18,16 +19,24 @@ elif os.getenv('ENV') == 'dev':
 elif os.getenv('ENV') == 'ghci':
     print("Running in github mode")
     app.config.from_object('config.GithubCIConfig')
-else:
-    print("Running in production mode")
-    app.config.from_object('config.ProductionConfig')
-
+elif os.getenv('ENV') == 'uat':
+    print("Running in UAT mode")
+    app.config.from_object('config.UATConfig')
 
 db = SQLAlchemy(app)
 
 from iebank_api.models import Account
+
 with app.app_context():
     db.create_all()
 CORS(app)
 
 from iebank_api import routes
+
+# Initialize Application Insights and force flushing application insights handler after each request
+if(os.getenv('ENV') == 'dev' or os.getenv('ENV') == 'uat'):
+    appinsights = AppInsights(app)
+    @app.after_request
+    def after_request(response):
+        appinsights.flush()
+        return response
