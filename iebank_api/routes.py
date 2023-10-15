b8ff0858bffd6cbfe5ef95d533a2942eb9cedd4e
cheblimarc4 <cheblimarc4@gmail.com>
2023-10-15 16:57:01 +0200

    donnee

--- iebank_api/routes.py
+++ iebank_api/routes.py
@@ -4,18 +4,12 @@ from iebank_api.models import Account
 
 @app.route('/')
 def hello_world():
-    # app.logger.debug('This is a debug log message')
-    # app.logger.info('This is an information log message')
-    # app.logger.warn('This is a warning log message')
-    # app.logger.error('This is an error message')
-    # app.logger.critical('This is a critical message')
-    app.logger.debug('Route / called')
     return 'Hello, World!'
 
 @app.route('/skull', methods=['GET'])
 def skull():
-    app.logger.debug('Route /skull GET called')
     text = 'Hi! This is the BACKEND SKULL! 💀 '
+    
     text = text +'<br/>Database URL:' + db.engine.url.database
     if db.engine.url.host:
         text = text +'<br/>Database host:' + db.engine.url.host
@@ -30,10 +24,9 @@ def skull():
 
 @app.route('/accounts', methods=['POST'])
 def create_account():
-    app.logger.debug('Route /accounts POST called')
     name = request.json['name']
-    country = request.json['country']
     currency = request.json['currency']
+    country = request.json['country']
     account = Account(name, currency, country)
     db.session.add(account)
     db.session.commit()
@@ -41,19 +34,16 @@ def create_account():
 
 @app.route('/accounts', methods=['GET'])
 def get_accounts():
-    app.logger.debug('Route /accounts GET')
     accounts = Account.query.all()
     return {'accounts': [format_account(account) for account in accounts]}
 
 @app.route('/accounts/<int:id>', methods=['GET'])
 def get_account(id):
-    app.logger.debug('Route /accounts GET called with id: ' + str(id))
     account = Account.query.get(id)
     return format_account(account)
 
 @app.route('/accounts/<int:id>', methods=['PUT'])
 def update_account(id):
-    app.logger.debug('Route /accounts PUT called with id: ' + str(id))
     account = Account.query.get(id)
     account.name = request.json['name']
     db.session.commit()
@@ -61,7 +51,6 @@ def update_account(id):
 
 @app.route('/accounts/<int:id>', methods=['DELETE'])
 def delete_account(id):
-    app.logger.debug('Route /accounts DELETE called with id: ' + str(id))
     account = Account.query.get(id)
     db.session.delete(account)
     db.session.commit()
